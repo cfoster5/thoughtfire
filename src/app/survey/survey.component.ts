0b18bef9354ccbf24c4898f84912163a14f6d25f
cfoster5 <cfoster3204@gmail.com>
2018-07-21 22:24:46 -0500

    Commit for survey to go from dropdowns to buttons

--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -28,12 +28,13 @@ export class SurveyComponent implements OnInit {
 
   counter: number = 0;
 
-  valueCount: number;
+  selections = [];
 
   constructor(public userSvc: UserinfoService, private router: Router, private route: ActivatedRoute, public ngProgress: NgProgress, public questions: Questions) {
     console.log("userInfo", this.userSvc.userInfo)
     console.log(localStorage.getItem('firstName'), localStorage.getItem('lastName'), localStorage.getItem('email'))
-    if (!this.userSvc.userInfo) {
+    if (!this.userSvc.userInfo && !localStorage.getItem('firstName')) {
+    // if (!localStorage.getItem('firstName')) {
         this.router.navigate(['../'], { relativeTo: this.route })
     }
     // this.ngProgress.set(0);
@@ -43,68 +44,54 @@ export class SurveyComponent implements OnInit {
   ngOnInit() {
   }
 
-  change(event) {
-    var selects = event.getElementsByTagName('select')
-    console.log(selects)
-
-    this.valueCount = 0
-    for (let i = 0; i < selects.length; i++) {
-      this.valueCount += parseInt(selects[i].value);
-      if (this.counter != 0 && this.counter != Number(1) && this.counter != Number(4) && this.counter != Number(9) && this.counter != Number(13) && this.counter != Number(16)) {
-        console.log("will count")
-        if (i == 0) {
-          this.col1 += parseInt(selects[i].value);
-        }
-        if (i == 1) {
-          this.col2 += parseInt(selects[i].value);
-        }
-        if (i == 2) {
-          this.col3 += parseInt(selects[i].value);
-        }
-        if (i == 3) {
-          this.col4 += parseInt(selects[i].value);
-        }
-      }
-    }
-    console.log("valueCount", this.valueCount)
-
-    console.log("col1 sum", this.col1)
-    console.log("col2 sum", this.col2)
-    console.log("col3 sum", this.col3)
-    console.log("col4 sum", this.col4)
-  }
-
   continuePressed() {
     let data = {
-      firstName: this.userSvc.userInfo.firstName,
-      lastName: this.userSvc.userInfo.lastName,
-      email: this.userSvc.userInfo.email,
+      // firstName: this.userSvc.userInfo.firstName,
+      // lastName: this.userSvc.userInfo.lastName,
+      // email: this.userSvc.userInfo.email,
+      firstName: localStorage.getItem('firstName'),
+      lastName: localStorage.getItem('lastName'),
+      email: localStorage.getItem('email'),
       col1: this.col1,
       col2: this.col2,
       col3: this.col3,
       col4: this.col4
     }
 
-    if (this.valueCount == 10) {
+    if (this.selections.length == 4) {
+
+      this.selections = [];
+
+      console.log(this.col1, this.col2, this.col3, this.col4)
+
       this.counter += 1;
       console.log(this.counter)
       this.ngProgress.set(this.counter/this.questions.questions.length);
-      this.valueCount = 0
 
-      if (this.counter == this.questions.questions.length) {
-        firebase.database().ref('surveys/').push(data);
+      if (this.counter != 0 && this.counter != Number(1) && this.counter != Number(4) && this.counter != Number(9) && this.counter != Number(13) && this.counter != Number(16)) {
 
-        this.userSvc.userInfo.col1 = this.col1;
-        this.userSvc.userInfo.col2 = this.col2;
-        this.userSvc.userInfo.col3 = this.col3;
-        this.userSvc.userInfo.col4 = this.col4;
+        let spans = document.getElementsByTagName('span')
 
-        localStorage.setItem('col1', this.userSvc.userInfo.col1);
-        localStorage.setItem('col2', this.userSvc.userInfo.col2);
-        localStorage.setItem('col3', this.userSvc.userInfo.col3);
-        localStorage.setItem('col4', this.userSvc.userInfo.col4);
+        this.col1 += parseInt(spans[0].innerText);
+        this.col2 += parseInt(spans[1].innerText);
+        this.col3 += parseInt(spans[2].innerText);
+        this.col4 += parseInt(spans[3].innerText);
 
-        this.router.navigate(['/profile'], { relativeTo: this.route })
+        if (this.counter == this.questions.questions.length) {
+          firebase.database().ref('surveys/').push(data);
+
+          this.userSvc.userInfo.col1 = this.col1;
+          this.userSvc.userInfo.col2 = this.col2;
+          this.userSvc.userInfo.col3 = this.col3;
+          this.userSvc.userInfo.col4 = this.col4;
+
+          localStorage.setItem('col1', this.userSvc.userInfo.col1);
+          localStorage.setItem('col2', this.userSvc.userInfo.col2);
+          localStorage.setItem('col3', this.userSvc.userInfo.col3);
+          localStorage.setItem('col4', this.userSvc.userInfo.col4);
+
+          this.router.navigate(['/profile'], { relativeTo: this.route })
+        }
       }
     }
   }
@@ -114,10 +101,34 @@ export class SurveyComponent implements OnInit {
       this.counter -= 1;
       console.log(this.counter)
       this.ngProgress.set(this.counter/this.questions.questions.length);
-      this.valueCount = 0
     }
     else {
       this.router.navigate(['/about'], { relativeTo: this.route })
+    }
+  }
+
+  makeSelection(event) {
+    if (!this.selections.includes(event.target.value)) {
+        console.log(event)
+        console.log(event.target.value)
+        this.selections.push(event.target.value)
+        console.log(this.selections)
+        console.log(this.selections.includes(event.target.value))
+
+        console.log("col1 sum", this.col1)
+        console.log("col2 sum", this.col2)
+        console.log("col3 sum", this.col3)
+        console.log("col4 sum", this.col4)
+    }
+  }
+
+  resetSelections(){
+    let dialogue = confirm("Would you like to reset your selections for this group?");
+    if (dialogue == true) {
+        this.selections = [];
+        console.log("You pressed OK!");
+    } else {
+        console.log("You pressed Cancel!");
     }
   }
 
